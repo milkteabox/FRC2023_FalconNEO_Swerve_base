5cfed6869e5817beb4973cdd2fed2e8711308e75
milkteabox <milkteabox.tu@gmail.com>
2023-02-19 19:12:57 +0800

    Initial commit

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SerialPort;
+
+public class RobotMap {
+    public static final String SWERVE_CANBUS_TYPE = "rio";
+
+    public static final int SWERVE_LEFTFRONT_DRIVEMOTOR = 1;
+    public static final int SWERVE_LEFTFRONT_ANGLEMOTOR = 2;
+    public static final int SWERVE_LEFTFRONT_CANCODER = 1;
+
+    public static final int SWERVE_LEFTREAR_DRIVEMOTOR = 3;
+    public static final int SWERVE_LEFTREAR_ANGLEMOTOR = 4;
+    public static final int SWERVE_LEFTREAR_CANCODER = 2;
+
+    public static final int SWERVE_RIGHTFRONT_DRIVEMOTOR = 7;
+    public static final int SWERVE_RIGHTFRONT_ANGLEMOTOR = 8;
+    public static final int SWERVE_RIGHTFRONT_CANCODER = 4;
+
+    public static final int SWERVE_RIGHTREAR_DRIVEMOTOR = 5;
+    public static final int SWERVE_RIGHTREAR_ANGLEMOTOR = 6;
+    public static final int SWERVE_RIGHTREAR_CANCODER = 3;
+
+}
